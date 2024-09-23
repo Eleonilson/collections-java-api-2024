f4a05c6829a0a2302e3210de464666a41650e554
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-09-22 22:25:59 -0300

    Ordenação em List

---
+++ src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,35 @@
+package main.java.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    //atributos
+
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        // Essa arrayList não vai iniciar vazia.
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparadorPorAltura());
+        return pessoasPorAltura;
+    }
+
+
+}
