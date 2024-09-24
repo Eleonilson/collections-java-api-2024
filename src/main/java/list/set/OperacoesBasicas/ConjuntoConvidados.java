e6a0abeb6a191c6b957bc4cf45dd3a48e001472c
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-09-24 00:02:53 -0300

    Operações Básicas Set: testando os métodos
    Signed-off-by: Eleonilson <eleonilson.r.l@gmail.com.br>

--- src/main/java/list/set/OperacoesBasicas/ConjuntoConvidados.java
+++ src/main/java/list/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -34,4 +34,24 @@ public class ConjuntoConvidados {
     public void exibirConvidados() {
         System.out.println(convidadoSet);
     }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados =new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarconvidados() + " convidado(s) dentro do set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidados 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidados 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidados 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidados 4", 1236);
+
+
+        conjuntoConvidados.exibirConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarconvidados() + " convidado(s) dentro do set de Convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1236);
+        System.out.println("Existem " + conjuntoConvidados.contarconvidados() + " convidado(s) dentro do set de Convidados");
+
+        conjuntoConvidados.exibirConvidados();
+    }
 }
--- src/main/java/list/set/OperacoesBasicas/Convidado.java
+++ src/main/java/list/set/OperacoesBasicas/Convidado.java
@@ -1,5 +1,7 @@
 package main.java.list.set.OperacoesBasicas;
 
+import java.util.Objects;
+
 public class Convidado {
     //atributos
 
@@ -17,6 +19,19 @@ public class Convidado {
 
     public int getCodigoConvite() {
         return codigoConvite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigoConvite());
     }
 
     @Override
