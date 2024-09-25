67918390381c645ec8ffd786e2457d55087c62f6
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-09-24 23:09:58 -0300

    Exercício conjunto de Palavras Únicas
    Signed-off-by: Eleonilson <eleonilson.r.l@gmail.com.br>

---
+++ src/main/java/list/set/OperacoesBasicas/ConjuntoPalavrasUnicas.java
@@ -0,0 +1,56 @@
+package main.java.list.set.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class ConjuntoPalavrasUnicas {
+    //atributos
+
+    private Set<PalavrasUnicas> palavrasUnicasset;
+
+    public ConjuntoPalavrasUnicas() {
+        this.palavrasUnicasset = new HashSet<>();
+    }
+
+    public void adicionarPalavra(String palavra) {
+        palavrasUnicasset.add(new PalavrasUnicas(palavra));
+    }
+
+    public void removerPalavra(String palavra) {
+        PalavrasUnicas removerPalavra = null;
+        for(PalavrasUnicas p : palavrasUnicasset) {
+            if(p.getPalavra().equals(palavra)) {
+                removerPalavra = p;
+                break;
+            }
+        }
+        palavrasUnicasset.remove(removerPalavra);
+    }
+
+    public boolean verificarPalavra(String palavra) {
+        return palavrasUnicasset.contains(new PalavrasUnicas(palavra));
+    }
+
+    public void exibirPalavrasUnicas() {
+        System.out.println(palavrasUnicasset);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoPalavrasUnicas conjuntoPalavrasUnicas = new ConjuntoPalavrasUnicas();
+        System.out.println("Palavras unicas: " + conjuntoPalavrasUnicas.palavrasUnicasset);
+
+        conjuntoPalavrasUnicas.adicionarPalavra("Casa");
+        conjuntoPalavrasUnicas.adicionarPalavra("Carro");
+        conjuntoPalavrasUnicas.adicionarPalavra("Bicicleta");
+        conjuntoPalavrasUnicas.adicionarPalavra("Casa");
+
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+
+        conjuntoPalavrasUnicas.removerPalavra("Carro");
+
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+
+        System.out.println("A palavra 'Bicicleta' está no conjunto? " + conjuntoPalavrasUnicas.verificarPalavra("Bicicleta"));
+    }
+}
---
+++ src/main/java/list/set/OperacoesBasicas/PalavrasUnicas.java
@@ -0,0 +1,35 @@
+package main.java.list.set.OperacoesBasicas;
+
+import java.util.Objects;
+
+public class PalavrasUnicas {
+    //atributos
+
+    private String palavra;
+
+    public PalavrasUnicas(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PalavrasUnicas that = (PalavrasUnicas) o;
+        return Objects.equals(getPalavra(), that.getPalavra());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getPalavra());
+    }
+
+    @Override
+    public String toString() {
+        return palavra;
+    }
+}
